27dbdd6c4fef620d8b445c4006f271e071dbc75d
Singuuu <jota.llopis@pm.me>
2023-03-10 10:53:44 +0100

    Add __str__ and __repr__

---
+++ str_and_repr.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"Person {self.name}, {self.age} years old."
+
+    def __repr__(self):
+        return f"<Person({self.name}, {self.age})>"
+
+
+benito = Person("Benito", 25)
+print(benito)
