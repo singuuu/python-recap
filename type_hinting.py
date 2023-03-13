40f01f878be001a77ca89c1c7a78a0f5e18dbe96
Singuuu <jota.llopis@pm.me>
2023-03-13 09:05:48 +0100

    Add type hinting

---
+++ type_hinting.py
@@ -0,0 +1,35 @@
+class Book:
+    TYPES = ("Hardcover", "Paperback")
+
+    def __init__(self, name: str, book_type: str, weight: int):
+        self.name = name
+        self.book_type = book_type
+        self.weight = weight
+
+    def __repr__(self) -> str:
+        return f"<Book {self.name}, {self.book_type}, {self.weight}>"
+
+    @classmethod
+    def hardcovers(cls, name, weight) -> "Book":
+        return cls(name, cls.TYPES[0], weight + 100)
+
+    @classmethod
+    def paperback(cls, name, weight) -> "Book":
+        return cls(name, cls.TYPES[1], weight + 100)
+
+    @classmethod
+    def pdf(cls, name, weight) -> "Book":
+        return cls(name, cls.pdf_type(), weight + 100)
+
+    @staticmethod
+    def pdf_type() -> str:
+        return "PDF"
+
+
+book = Book.hardcovers("Harry Potter", 1500)
+light = Book.paperback("Python 101", 600)
+pdf = Book.pdf("Python for pentesters", 1000)
+
+print(book)
+print(light)
+print(pdf)
